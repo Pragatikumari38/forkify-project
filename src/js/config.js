6900364b442dbf720a8c5b95dcaf70cf61642849
Pragatikumari38 <pragatigautam38@gmail.com>
2024-09-20 08:45:50 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const KEY = '774ec35e-ccf6-4d8b-ac1c-f157fe9e626a';
+export const MODAL_CLOSE_SEC = 2.5;
